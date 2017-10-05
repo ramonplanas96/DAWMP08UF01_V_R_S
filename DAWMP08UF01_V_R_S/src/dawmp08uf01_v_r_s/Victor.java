e707fe6b1ac302fef1d2d7c77fb5e21eedd44b4f
victo <victo@DESKTOP-HLVKSAK>
2017-10-05 21:13:50 +0200

    Creo los tres metodos victor1,victor2,victor3.

---
+++ DAWMP08UF01_V_R_S/src/dawmp08uf01_v_r_s/Victor.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dawmp08uf01_v_r_s;
+
+/**
+ *
+ * @author victo
+ */
+public class Victor {
+    
+    private void metodoVictor1(){
+        
+    }
+    
+    private void metodoVictor2(){
+        
+    }
+    
+    public void metodoVictor3(){
+        
+    }
+    
+    
+}
