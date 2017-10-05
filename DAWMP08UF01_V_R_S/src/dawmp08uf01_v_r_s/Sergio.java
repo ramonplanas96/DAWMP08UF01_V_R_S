c19665387670ea581b93850c8ddfe37e53a83e5a
el_19 <el_19@DESKTOP-RNGFDGB>
2017-10-05 21:12:02 +0200

    MetodosSergio creados!

---
+++ DAWMP08UF01_V_R_S/src/dawmp08uf01_v_r_s/Sergio.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dawmp08uf01_v_r_s;
+
+/**
+ *
+ * @author el_19
+ */
+public class Sergio {
+    
+   public void metodoSergio1 () {
+   } 
+   
+   protected void metodoSergio2 () {
+   } 
+   
+   private void metodoSergio3 () {
+   } 
+}
+
+
+
