a23b3472c82b3583493e33df1545e6c839e778ea
el_19 <el_19@DESKTOP-RNGFDGB>
2017-10-05 21:33:04 +0200

    MetodosSergio creados y con codigo!

---
+++ DAWMP08UF01_V_R_S/src/dawmp08uf01_v_r_s/Ramon.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dawmp08uf01_v_r_s;
+
+/**
+ *
+ * @author RamonMV
+ */
+public class Ramon {
+    
+    public String metodoRamon1(){
+        return "Hola";
+    }
+    
+    private void metodoRamon2(){
+        
+    }
+    
+    protected int metodoRamon3(){
+        return 10;
+    }
+}
